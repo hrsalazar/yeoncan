b659fd5c7f7536ef01ff92bfd64722028035db88
Hector R. Salazar <hector.sa@gmail.com>
2013-08-20 18:17:57 -0500

    Inicializando repositorio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yeoncan::Application.config.secret_key_base = '9d61446e9ca3c92172c197d19ea173f1e9260cfca1846ed47ee89bd3cd1278f70f43fbd4c43e80c99ac66bd1649e5a8a2f3947c2ef8facf49bce6f47dc355028'
